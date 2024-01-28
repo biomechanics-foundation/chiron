f674b96f37c0cdae70e9cfbccbc1d1923cc6b2e2
Claire V. Hammond <cvhammond@mailbox.org>
2024-01-28 01:40:53 -0800

    fix load new c3d

--- src/c3dio/gui/visualizer/c3d.rs
+++ src/c3dio/gui/visualizer/c3d.rs
@@ -16,6 +16,7 @@ pub fn c3d_drag_and_drop(
                 notifications.remove_overlay();
                 c3d_state.path = path_buf.to_str().unwrap().to_string();
                 c3d_state.handle = asset_server.load(&c3d_state.path);
+                c3d_state.loaded = false;
             }
             FileDragAndDrop::HoveredFile { window, path_buf } => {
                 notifications.overlay(Icon::Upload, true);
@@ -31,45 +32,20 @@ pub fn load_c3d(
     mut events: EventReader<C3dLoadedEvent>,
     c3d_state: ResMut<C3dState>,
     c3d_assets: Res<Assets<C3dAsset>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut commands: Commands,
+    mut c3d_frame: ResMut<C3dFrame>,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+    commands: Commands,
     mut notifications: ResMut<Notifications>,
+    markers: Query<Entity, With<Marker>>,
 ) {
     if let Some(_) = events.read().last() {
         let asset = c3d_assets.get(&c3d_state.handle);
         match asset {
             Some(asset) => {
                 notifications.add(Toast::success("C3D file loaded successfully"));
-                for i in 0..asset.c3d.points.labels.len() {
-                    let matrix = Mat4::from_scale_rotation_translation(
-                        Vec3::new(1.0, 1.0, 1.0),
-                        Quat::from_rotation_y(0.0),
-                        Vec3::new(
-                            asset.c3d.points.points[0][i][0] as f32 / 1000.0,
-                            asset.c3d.points.points[0][i][1] as f32 / 1000.0,
-                            asset.c3d.points.points[0][i][2] as f32 / 1000.0,
-                        ),
-                    );
-                    commands.spawn((
-                        PbrBundle {
-                            mesh: meshes.add(
-                                shape::UVSphere {
-                                    radius: 0.01,
-                                    ..default()
-                                }
-                                .into(),
-                            ),
-                            material: materials.add(StandardMaterial {
-                                base_color: Color::rgb_u8(255, 0, 255),
-                                ..default()
-                            }),
-                            transform: Transform::from_matrix(matrix),
-                            ..default()
-                        },
-                        Marker,
-                    ));
-                }
+                add_markers(&asset, commands, meshes, materials, markers);
+                c3d_frame.update_frame(0.);
             }
             None => {}
         }
@@ -96,6 +72,9 @@ pub fn markers(
         Some(asset) => {
             let point_data = &asset.c3d.points.points;
             let frame = c3d_frame.frame() as usize;
+            if frame >= point_data.rows() {
+                return;
+            }
             for (i, (mut transform, _)) in query.iter_mut().enumerate() {
                 transform.translation = Vec3::new(
                     point_data[frame][i][0] as f32 / 1000.0,
@@ -105,5 +84,46 @@ pub fn markers(
             }
         }
         None => {}
+    }
+}
+
+pub fn add_markers(
+    asset: &C3dAsset,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    markers: Query<Entity, With<Marker>>,
+) {
+    for marker in markers.iter() {
+        commands.entity(marker).despawn();
+    }
+    for i in 0..asset.c3d.points.labels.len() {
+        let matrix = Mat4::from_scale_rotation_translation(
+            Vec3::new(1.0, 1.0, 1.0),
+            Quat::from_rotation_y(0.0),
+            Vec3::new(
+                asset.c3d.points.points[0][i][0] as f32 / 1000.0,
+                asset.c3d.points.points[0][i][1] as f32 / 1000.0,
+                asset.c3d.points.points[0][i][2] as f32 / 1000.0,
+            ),
+        );
+        commands.spawn((
+            PbrBundle {
+                mesh: meshes.add(
+                    shape::UVSphere {
+                        radius: 0.01,
+                        ..default()
+                    }
+                    .into(),
+                ),
+                material: materials.add(StandardMaterial {
+                    base_color: Color::rgb_u8(255, 0, 255),
+                    ..default()
+                }),
+                transform: Transform::from_matrix(matrix),
+                ..default()
+            },
+            Marker,
+        ));
     }
 }
